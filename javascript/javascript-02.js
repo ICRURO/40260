26fbdd31802505896fc877841d2f17e44b898f33
ICRURO <zs24013397@estudiantes.uv.mx>
2026-05-18 12:12:03 -0600

    js con y sin modulos

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,4 +1,4 @@
-let resultado = document.getElementById("resultado");
+/*let resultado = document.getElementById("resultado");
 let botones = document.getElementsByTagName("button");
 let prm1 = 0;
 let operador = "";
@@ -63,4 +63,141 @@ function igual() {
     resultado.value = res;
     operador = "";
     prm1 = 0;
+}*/
+
+let resultado = document.getElementById("resultado")
+let botones = document.getElementsByTagName("button")
+for (const key in botones) {
+    if (Object.prototype.hasOwnProperty.call(botones, key)){
+        const boton = botones[key];
+        if (boton.className != "operadores")
+            boton.addEventListener("click", pintar)
+        else
+            boton.addEventListener("click", operadores)
+        //console.log(boton)
+
+    }
 }
+
+function pintar(e){
+    console.log(e.target.innerText)
+    resultado.value += e.target.innerText
+}
+
+// let operadores = document.getElementsByClassName("operadores")
+//     for (const key in operadores) {
+//         if (Object.prototype.hasOwnProperty.call(operadores, key)){
+//             const boton = operadores[key];
+//             boton.addEventListener("click", pintar2)
+//             //console.log(boton)
+
+//         }
+//     }
+
+function operadores(e){
+    let operador = e.target.innerText
+    if (operador == "=")
+        igual()
+    else
+        almacena_parametro(operador)
+}
+
+let prm1
+let operacion
+function almacena_parametro(prm){
+    //prm1 = resultado.value
+    parametros.push(resultado.value)
+    resultado.value = ""
+    operacion = prm
+
+}
+
+
+function igual(){
+    //prm2 = resultado.value
+    let variable = 0
+    parametros.push(resultado.value)
+    for (const key in parametros) {
+        if (Object.prototype.hasOwnProperty.call(parametros, key)) {
+
+                    
+            const prm = parametros[key];
+
+            if (operacion == "+")
+                variable += parseInt(prm)
+
+        
+        }
+
+        
+    }
+
+    resultado.value=variable
+    // if (operacion == "+")
+    //     resultado.value = parseInt(prm1) + parseInt(prm2)
+    // if (operacion == "-")
+    //     resultado.value = parseInt(prm1) - parseInt(prm2)
+    // if (operacion == "*")
+    //     resultado.value = parseInt(prm1) * parseInt(prm2)
+    // if (operacion == "/")
+    //     resultado.value = parseInt(prm1) / parseInt(prm2)
+}
+
+let parametros = []
+
+
+// let resultado = document.getElementById("resultado");
+// let botones = document.getElementsByTagName("button");
+
+// // Variables para almacenar los valores y la operación
+// let prm1 = 0;
+// let operacion = "";
+
+// for (const key in botones) {
+//     if (Object.prototype.hasOwnProperty.call(botones, key)) {
+//         const boton = botones[key];
+//         if (boton.className != "operadores")
+//             boton.addEventListener("click", pintar);
+//         else
+//             boton.addEventListener("click", pintar2);
+//     }
+// }
+
+// function pintar(e) {
+//     resultado.value += e.target.innerText;
+// }
+
+// function pintar2(e) {
+//     let signo = e.target.innerText;
+    
+//     if (signo === "=") {
+//         igual();
+//     } else if (signo === "Ce") {
+//         limpiar();
+//     } else {
+//         // Guardamos el primer número y el tipo de operación
+//         prm1 = resultado.value;
+//         operacion = signo;
+//         resultado.value = "";
+//     }
+// }
+
+// function igual() {
+//     let prm2 = resultado.value;
+//     let calculo = 0;
+//     let n1 = parseFloat(prm1);
+//     let n2 = parseFloat(prm2);
+
+//     if (operacion === "+") calculo = n1 + n2;
+//     if (operacion === "-") calculo = n1 - n2;
+//     if (operacion === "*") calculo = n1 * n2;
+//     if (operacion === "/") calculo = n1 / n2;
+
+//     resultado.value = calculo;
+// }
+
+// function limpiar() {
+//     resultado.value = "";
+//     prm1 = 0;
+//     operacion = "";
+// }
